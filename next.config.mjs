1064d8a814ecb74e39ce13fc2f22e0ce8024dc00
Wenting Li <liwenting0601@gmail.com>
2025-05-23 15:44:36 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/wenting-li-portfolio' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/wenting-li-portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/AboutMe' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/AboutMe' : '',
 }
 
 export default nextConfig
